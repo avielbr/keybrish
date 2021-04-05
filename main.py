fda2b0f18278454c182683c1bde3ecd240504e3f
aviel <abrown.inbox@gmail.com>
2021-04-05 23:05:10 +0300

    Working

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
+from keyboard import add_hotkey, wait
+from pyperclip import copy, paste
 from mappings import Mappings
+from pyautogui import hotkey
+from time import sleep
 
-test = "Hקט צשמ ים' שרק טםו??"
+def flip():
+    hotkey('ctrl', 'a')
+    hotkey('ctrl', 'c')
+    sleep(0.1)
 
-if ord(test[0]) > 128 or test[0].isupper():
-    print(test.translate(Mappings.heb_eng))
-else:
-    print(test.translate(Mappings.eng_heb)[::-1])
+    text = paste()
+
+    if ord(text[0]) > 128 or text[0].isupper():
+        copy(text.translate(Mappings.heb_eng))
+
+    else: 
+        copy(text.translate(Mappings.eng_heb))
+
+    hotkey('ctrl', 'v')
+
+if __name__ == '__main__':
+    add_hotkey('ctrl+\\', flip)
+    wait()
